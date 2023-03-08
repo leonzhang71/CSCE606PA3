bbc12b99796ceeb43d98e9b8eed77ed292d3bdcf
leonzhang71 <112522298+leonzhang71@users.noreply.github.com>
2023-03-08 01:45:25 -0600

    Delete PA3/rottenpotatoes directory

--- PA3/rottenpotatoes/app/models/movie.rb
+++
@@ -1,6 +0,0 @@
-class Movie < ActiveRecord::Base
-  #def find_similar_movies
-  #  return [] unless director.present?
-  #  Movie.where(director: director).where.not(id: id)
-  #end
-end
--- PA3/rottenpotatoes/db/migrate/20230306053716_add_director_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddDirectorToMovies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :movies, :director, :string
-  end
-end
